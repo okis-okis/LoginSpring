fad1b7c2319dfe9fed34781c1e224c412cf370f9
OKIS <67818930+okis-okis@users.noreply.github.com>
2023-09-07 10:23:02 +0300

    Add files via upload

---
+++ src/main/java/com/example/demo/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+	
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+        .authorizeRequests()
+        	.requestMatchers("/","/home")
+        	.permitAll()
+            .anyRequest().authenticated()
+            .and()
+        .formLogin()
+            .loginPage("/login") 
+            .permitAll()
+            .usernameParameter("username")
+            .defaultSuccessUrl("/secret")
+            .failureUrl("/login?error=true");   
+
+		return http.build();
+	}
+	
+	@Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
